121c3cf321afae2318659d31e7aef574439ba7b6
Akshay Nagpal <akshay2626@gmail.com>
2016-12-20 21:02:17 -0500

    some code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,16 @@
 package controllers;
-
-import play.*;
-import play.mvc.*;
-import play.db.jpa.*;
-import views.html.*;
-import models.Person;
+import models.Volunteer;
 import play.data.FormFactory;
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
+
 import javax.inject.Inject;
 import java.util.List;
 
-import static play.libs.Json.*;
+import static play.libs.Json.toJson;
 
 public class Application extends Controller {
 
@@ -22,14 +23,29 @@ public class Application extends Controller {
 
     @Transactional
     public Result addPerson() {
-        Person person = formFactory.form(Person.class).bindFromRequest().get();
-        JPA.em().persist(person);
+        Volunteer volunteer = formFactory.form(Volunteer.class).bindFromRequest().get();
+        JPA.em().persist(volunteer);
         return redirect(routes.Application.index());
     }
 
     @Transactional(readOnly = true)
     public Result getPersons() {
-        List<Person> persons = (List<Person>) JPA.em().createQuery("select p from Person p").getResultList();
-        return ok(toJson(persons));
+        List<Volunteer> volunteers = (List<Volunteer>) JPA.em().createQuery("select p from Person p").getResultList();
+        return ok(toJson(volunteers));
+    }
+
+    public Result showVolunteerPage(){
+        return ok(volunteer.render());
+    }
+
+    public Result showOrganizationPage(){
+        return ok(organization.render());
+    }
+
+    public Result showVolunteerSearchPage(){
+        return ok(searchVolunteer.render());
+    }
+    public Result showOrganizationSearchPage(){
+        return ok(searchOrganization.render());
     }
 }
---
+++ app/models/Volunteer.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created by akshay on 12/20/2016.
+ */
+public class Volunteer {
+
+    Volunteer(){
+
+    }
+}
