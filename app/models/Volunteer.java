3de29450d7c21e7b5474b20f6cfdde4546fce055
Akshay Nagpal <akshay2626@gmail.com>
2016-12-23 01:16:36 -0500

    getters and setters volunteer

--- app/models/Volunteer.java
+++ app/models/Volunteer.java
@@ -13,4 +13,52 @@ public class Volunteer {
         this.yearsExperience = yearsExperience;
         this.location = location;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public String getIssues() {
+        return issues;
+    }
+
+    public String getYearsExperience() {
+        return yearsExperience;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public void setIssues(String issues) {
+        this.issues = issues;
+    }
+
+    public void setYearsExperience(String yearsExperience) {
+        this.yearsExperience = yearsExperience;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
