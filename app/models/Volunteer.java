b2d4556c6067b284b7d3688c97c8fa5423e5aace
Akshay Nagpal <akshay2626@gmail.com>
2016-12-23 01:13:22 -0500

    created volunteer class

--- app/models/Volunteer.java
+++ app/models/Volunteer.java
@@ -4,7 +4,13 @@ package models;
  * Created by akshay on 12/20/2016.
  */
 public class Volunteer {
-    Volunteer(){
-
+    String firstName,lastName,skills,issues,yearsExperience,location;
+    Volunteer(String firstName, String lastname,String skills,String issues,String yearsExperience,String location){
+        this.firstName = firstName;
+        this.lastName = lastname;
+        this.skills = skills;
+        this.issues = issues;
+        this.yearsExperience = yearsExperience;
+        this.location = location;
     }
 }
