6a88ff597fe9f0cfd29e93e1ed36b5be2369a6c4
Abhijeet Mehrotra <abhijeet.meh@gmail.com>
2016-12-23 17:07:34 -0500

    massive integration of dull flow

--- app/models/Volunteer.java
+++ app/models/Volunteer.java
@@ -4,8 +4,9 @@ package models;
  * Created by akshay on 12/20/2016.
  */
 public class Volunteer {
-    String firstName,lastName,skills,issues,yearsExperience,location;
-    Volunteer(String firstName, String lastname,String skills,String issues,String yearsExperience,String location){
+    public String firstName,lastName,skills,issues,yearsExperience,location;
+
+    public Volunteer(String firstName, String lastname, String skills, String issues, String yearsExperience, String location){
         this.firstName = firstName;
         this.lastName = lastname;
         this.skills = skills;
